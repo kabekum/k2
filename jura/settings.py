460b7a2cdffb3667162f5afc1157233355ee84cb
kabekum <98370249+kabekum@users.noreply.github.com>
2025-12-28 18:03:34 +0300

    Remove commented-out tasks app from settings

--- jura/settings.py
+++ jura/settings.py
@@ -34,9 +34,9 @@ INSTALLED_APPS = [
     'apps.matters',
     'apps.documents',
     'apps.billing',
-    'apps.tasks',
+    #'apps.tasks',
 ]
-]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,5 +120,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
