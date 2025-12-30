6570af289ed052368f3bc50bc01f01c1226f1ba3
kabekum <98370249+kabekum@users.noreply.github.com>
2025-12-30 11:39:46 +0300

    Update settings.py

--- jura/settings.py
+++ jura/settings.py
@@ -140,7 +140,14 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
+
 
 
 
