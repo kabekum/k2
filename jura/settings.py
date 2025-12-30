fb022ac42f844084c8fe716eea8e4db636976448
kabekum <98370249+kabekum@users.noreply.github.com>
2025-12-30 08:52:29 +0300

    Update INSTALLED_APPS to include ComplianceConfig

--- jura/settings.py
+++ jura/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'apps.compliance.apps.ComplianceConfig',
 
     'apps.accounts',
     'apps.clients',
@@ -138,6 +139,7 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
 
 
 
