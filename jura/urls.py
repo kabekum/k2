a00cb9b7567820f53188e2d93742aabfa4d0b61d
kabekum <98370249+kabekum@users.noreply.github.com>
2025-12-30 09:08:20 +0300

    Update urls.py

--- jura/urls.py
+++ jura/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from apps.matters.views import MatterViewSet
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from apps.discovery.views import EvidenceViewSet
 
 
 router = DefaultRouter()
 router.register('matters', MatterViewSet)
-router.register('discovery/evidence', EvidenceViewSet)
+router.register(r'discovery/evidence', EvidenceViewSet, basename='evidence')
 
 
 urlpatterns = [
